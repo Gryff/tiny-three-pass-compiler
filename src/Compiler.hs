abe78ac407da39d02efe81ac005b4ced07bf32a6
Liam Griffin <gryffskin@gmail.com>
2018-05-28 22:19:43 +0100

    Add tokenise helper function from codewars

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -8,5 +8,26 @@ data AST = Imm Int
          | Div AST AST
          deriving (Eq, Show)
 
+data Token = TChar Char
+           | TInt Int
+           | TStr String
+           deriving (Eq, Show)
+
 pass1 :: String -> AST
 pass1 x = Imm $ read (last (words x))
+
+tokenise :: String -> [Token]
+tokenise  [] = []
+tokenise xxs@(c:cs)
+  | c `elem` "-+*/()[]" = TChar c : tokenise cs
+  | not (null i) = TInt (read i) : tokenise is
+  | not (null s) = TStr s : tokenise ss
+  | otherwise = tokenise cs
+  where
+    (i, is) = span (`elem` digit) xxs
+    (s, ss) = span (`elem` alpha) xxs
+
+alpha, digit :: String
+alpha = ['a'..'s'] ++ ['A'..'z']
+digit = ['0'..'9']
+
